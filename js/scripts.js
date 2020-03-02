98ff7742e1ac633010eb5b61e72f2200f83ab75e
candycrushpro <bradleynilmar6702@gmail.com>
2020-03-02 16:59:29 +0300

    toppings to be added on  your pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
- $(document).ready(function(){
+ 
  function getSizeCost() {
     var selectedSize = document.getElementById("size").value;
     return parseInt(selectedSize);
@@ -63,10 +63,6 @@
   
   }
   
-  // Delivery form inputs and alerts
-  
-  
-  
   $(document).ready(function () {
     $("#delivery").submit(function () {
   
@@ -74,11 +70,10 @@
         var number = $("input#number").val();
         var location = $("input#location").val();
   
-        alert("Hello " + name + ". Your order has been successfuly received and will be delivered to " +  location + " within one hour.The delivery will cost ksh 180/= Thank you for chosing the pizzeria.");
-        // $(this).get(0).reset();
-        //  event.preventDefault();
+        prompt("Hello " + name + ". Your order has been successfuly received and will be delivered to " +  location + " within one hour.The delivery will cost ksh 180/= Thank you for chosing the pizzeria.");
+    
     });
     
   });
-})
+
   
